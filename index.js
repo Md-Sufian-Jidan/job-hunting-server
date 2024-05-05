e40fffa03c2b8f063f9c0c19069cf0cdf326998d
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-05-05 11:54:36 +0600

    delete a data by id api created

--- index.js
+++ index.js
@@ -50,6 +50,13 @@ async function run() {
             const query = { 'buyer.email': email };
             const result = await jobsCollection.find(query).toArray();
             res.send(result);
+        });
+        // delete a job data from db
+        app.delete('/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await jobsCollection.deleteOne(query);
+            res.send(result);
         })
 
         //save a bid data  in db
