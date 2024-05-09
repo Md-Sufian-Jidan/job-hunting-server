0c0c6cac4f95dd8b04d5b8ee66bbd91593744b1c
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-05-09 20:08:12 +0600

    you can not add duplicate data api added

--- index.js
+++ index.js
@@ -128,6 +128,13 @@ async function run() {
         //save a bid data  in db
         app.post('/bid', async (req, res) => {
             const bidData = req.body;
+
+            // check if it's a duplicate request
+            const query = { email: bidData.email, jobId: bidData.jobId }
+            const alreadyApplied = await bidsCollection.findOne(query);
+            if (alreadyApplied) {
+                return res.status(400).send('You have already placed a bid on this job')
+            }
             const result = await bidsCollection.insertOne(bidData);
             res.send(result);
         });
